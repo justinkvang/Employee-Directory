6be74feb531c85fdf5a6e5ae1181499f43b9b21b
JMan <justinvang07@gmail.com>
2021-03-02 21:33:11 -0600

    got working components from a class activity but API still not linking correctly

--- src/components/RandomUserContainer.js
+++ src/components/RandomUserContainer.js
@@ -1,17 +1,69 @@
 import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import ResultList from "./ResultList";
 import API from "../utils/API";
 
 class RandomUserContainer extends Component {
-    state = {
-        result: {},
-        search: ""
-    };
+  state = {
+    search: "",
+    results: []
+  };
 
-    searchUser = query => {
-        API.search(query)
-        .then(res => this.setState({ result: res.data}))
-        .catch(err => console.log(err));
-    };
+  // When this component mounts, search the Giphy API for pictures of kittens
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=200")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  searchGiphy = query => {
+    API.search(query)
+      .then(res => this.setState({ results: res.data.data }))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the Giphy API for `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchGiphy(this.state.search);
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results} />
+      </div>
+    );
+  }
 }
 
 export default RandomUserContainer;
