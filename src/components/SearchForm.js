7a6ce3996cc14125191f3344d8aa98ddb01897d7
JMan <justinvang07@gmail.com>
2021-03-03 22:21:15 -0600

    updated search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,18 +3,17 @@ import React from "react";
 function SearchForm(props) {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="search">Search:</label>
+      <div className="input-group mb-3">
         <input
           onChange={props.handleInputChange}
           value={props.search}
           name="search"
           type="text"
           className="form-control"
-          placeholder="Search for a Gif"
+          placeholder="Search for a Employee"
           id="search"
         />
-        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+        <button onClick={props.handleFormSubmit} className="btn btn-outline-secondary">
           Search
         </button>
       </div>
