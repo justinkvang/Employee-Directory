7d810999d724944d9b94061820302868477c33ab
JMan <justinvang07@gmail.com>
2021-03-03 22:13:24 -0600

    update

--- src/components/RandomUser.js
+++ src/components/RandomUser.js
@@ -8,35 +8,7 @@ class RandomUser extends Component {
     user: []
   };
 
-  // When this component mounts, search the Giphy API for pictures of kittens
-  componentDidMount() {
-    fetch("https://randomuser.me/api/?results=200")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  searchUser = query => {
-    API.search(query)
-      .then(res => this.setState({ results: res.data.data }))
-      .catch(err => console.log(err));
-  };
-
+  
   render() {
     return (
       <>
